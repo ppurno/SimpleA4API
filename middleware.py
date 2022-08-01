1d22b72da3ae5b5f4b063756bd69480d91153a1b
ppurno <82290761+ppurno@users.noreply.github.com>
2022-08-01 11:08:34 +0100

    Update middleware.py

--- middleware.py
+++ middleware.py
@@ -4,9 +4,44 @@
 """
 Docstring: 
 """
+from os import abort
+
+from flask import jsonify, request
+
+heroes = [{'name': 'John Smith', 'nationality': 'British', 'occupation': 'Doctor'},
+          {'name': 'Elon Musk', 'nationality': 'South African', 'occupation': 'Entrepreneur'},
+          {'name': 'Bob', 'nationality': 'USA', 'occupation': 'Pilot'}
+          ]
+
+
 def index():
     return "Hello World"
 
 
 def db():
     return "Hello DB grads"
+
+
+def user_profile(id):
+    return f"Profile page of user #{id}"
+
+
+def hero():
+    return jsonify(heroes)
+
+
+def hero_add():
+    try:
+        data = request.get_json(force=True)
+        name = data['name']
+        nationality = data['nationality']
+        occupation = data['occupation']
+        if name and nationality and occupation:
+            heroes.append({'name': name, 'nationality': nationality,
+                           'occupation': occupation})
+            return jsonify({"Hero "+ name: " Added successfully"})
+    except Exception as err:
+        print(err)
+        abort(400)
+    return None
+
