bbe141160de79b55e3fa7d52d721bbb62d8221a7
ppurno <82290761+ppurno@users.noreply.github.com>
2022-08-01 11:08:59 +0100

    Update routes.py

--- routes.py
+++ routes.py
@@ -6,7 +6,7 @@ Docstring:
 """
 from flask import jsonify
 
-from middleware import index, db
+from middleware import index, db, user_profile, hero, hero_add
 
 
 def initialise_routes(app):
@@ -14,6 +14,9 @@ def initialise_routes(app):
         app.add_url_rule('/api/hello/', 'index', index)
         app.add_url_rule('/api/db/', 'db', db)
         app.add_url_rule('/api/', 'list_routes', list_routes, methods=['GET'], defaults={'app': app})
+        app.add_url_rule('/api/profile/<id>/', 'user_profile', user_profile)
+        app.add_url_rule('/api/hero/', 'hero', hero)
+        app.add_url_rule('/api/hero/', 'hero_add', hero_add, methods=['POST'])
     return None
 
 
@@ -21,7 +24,7 @@ def list_routes(app):
     routes = []
     for route in app.url_map.iter_rules():
         routes.append({'Route': str(route),
-                       'Endpoint': route.endpoint(),
+                       'Endpoint': route.endpoint,
                        'Methods': list(route.methods)
                        })
     return jsonify({"Routes": routes, "Total": len(routes)})
